620f5effee66495e301988247284e1ed20e00a4d
Kevin Avila <kevin.avila@utexas.edu>
2017-11-14 00:24:23 -0600

    Created EditProfile controller

---
+++ Ellomix/EditProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileController.swift
+//  Ellomix
+//
+//  Created by Kevin Avila on 11/13/17.
+//  Copyright © 2017 Akshay Vyas. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileController: UITableViewController {
+    
+    override func viewDidLoad() {
+
+    }
+    
+    @IBAction func cancelEdit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
