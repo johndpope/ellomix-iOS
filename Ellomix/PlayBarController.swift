249cec95ed1fc43b1fccfbcce05c3ffe60bdfa63
Kevin Avila <kevin.avila@utexas.edu>
2017-12-19 00:45:12 -0600

    Added song labels and artwork image view to playbar

--- Ellomix/PlayBarController.swift
+++ Ellomix/PlayBarController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class PlayBarController: UIViewController {
     
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var playbarArtwork: UIImageView!
+    @IBOutlet weak var playbarTitle: UILabel!
+    @IBOutlet weak var playbarArtist: UILabel!
+    
     override func viewDidLoad() {
         
     }
