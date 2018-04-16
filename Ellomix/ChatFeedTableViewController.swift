9814abff2444cddbdd8719e7d1666bed008e72bd
Kevin Avila <kevin.avila@utexas.edu>
2018-04-16 00:10:12 -0500

    Refactored observers in Chat Feed View Controller. Message details now get updated in real time on the chat feed.

--- Ellomix/ChatFeedTableViewController.swift
+++ Ellomix/ChatFeedTableViewController.swift
@@ -11,7 +11,8 @@ import Firebase
 
 class ChatFeedTableViewController: UITableViewController {
     
-    private var groupsRefHandle: DatabaseHandle?
+    private var userGroupsRefHandle: DatabaseHandle?
+    private var currentChatObservers = Dictionary<String, DatabaseHandle>()
     private var FirebaseAPI: FirebaseApi!
 
     var currentUser:EllomixUser?
@@ -23,49 +24,71 @@ class ChatFeedTableViewController: UITableViewController {
         FirebaseAPI = FirebaseApi()
         
         currentUser = Global.sharedGlobal.user
-        observeChats()
+        
+        for gid in (self.currentUser?.groups)! {
+            let group = Group()
+            group.gid = gid
+            observeChat(group: group)
+            groupChats.append(group)
+        }
+        observeNewChats()
     }
     
     deinit {
-        if let refHandle = groupsRefHandle  {
+        if let refHandle = userGroupsRefHandle  {
             FirebaseAPI.getUsersRef().child((currentUser?.uid)!).child("groups").removeObserver(withHandle: refHandle)
+        }
+        
+        for (gid, handle) in currentChatObservers {
+            FirebaseAPI.getGroupsRef().child(gid).removeObserver(withHandle: handle)
         }
     }
     
-    func observeChats() {
-        groupsRefHandle = FirebaseAPI.getUsersRef().child((currentUser?.uid)!).child("groups").observe(.childAdded, with: { (snapshot) -> Void in
+    func observeNewChats() {
+        userGroupsRefHandle = FirebaseAPI.getUsersRef().child((currentUser?.uid)!).child("groups").observe(.childAdded, with: { (snapshot) -> Void in
             let gid = snapshot.key
-
-            self.FirebaseAPI.getGroupsRef().child(gid).observeSingleEvent(of: .value, with: { (snapshot) in
-                if let groupDictionary = snapshot.value as? Dictionary<String, AnyObject> {
-                    let group = Group()
-                    group.gid = gid
-                    group.name = groupDictionary["name"] as? String
-                    group.notifications = groupDictionary["notifications"] as? Bool
-                    if let users = groupDictionary["users"] as? Dictionary<String, AnyObject> {
-                        var usersArray = [Dictionary<String, AnyObject>?]()
-                        for user in users.values {
-                            usersArray.append(user as? Dictionary<String, AnyObject>)
-                        }
-                        group.users = usersArray
-                    }
-                    
-                    let lastMessage = Message()
-                    if let lastMessageDictionary = groupDictionary["last_message"] as? Dictionary<String, AnyObject> {
-                        lastMessage.content = lastMessageDictionary["content"] as? String
-                        lastMessage.timestamp = lastMessageDictionary["timestamp"] as? Int
-                        group.lastMessage = lastMessage
-                    }
-                    self.groupChats.append(group)
-                    
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-            })
-
+            
+            if (!(self.currentUser?.groups.contains(gid))!) {
+                let group = Group()
+                group.gid = gid
+                self.observeChat(group: group)
+                self.currentUser?.groups.append(gid)
+                self.groupChats.append(group)
+            }
         })
 
+    }
+    
+    func observeChat(group: Group) {
+        let handle = FirebaseAPI.getGroupsRef().child(group.gid!).observe(.value, with: { (snapshot) in
+            if let groupDictionary = snapshot.value as? Dictionary<String, AnyObject> {
+                self.setGroupProperties(group: group, groupDictionary: groupDictionary)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        })
+        
+        currentChatObservers[group.gid!] = handle
+    }
+    
+    func setGroupProperties(group: Group, groupDictionary: Dictionary<String, AnyObject>) {
+        group.name = groupDictionary["name"] as? String
+        group.notifications = groupDictionary["notifications"] as? Bool
+        if let users = groupDictionary["users"] as? Dictionary<String, AnyObject> {
+            var usersArray = [Dictionary<String, AnyObject>?]()
+            for user in users.values {
+                usersArray.append(user as? Dictionary<String, AnyObject>)
+            }
+            group.users = usersArray
+        }
+        
+        let lastMessage = Message()
+        if let lastMessageDictionary = groupDictionary["last_message"] as? Dictionary<String, AnyObject> {
+            lastMessage.content = lastMessageDictionary["content"] as? String
+            lastMessage.timestamp = lastMessageDictionary["timestamp"] as? Int
+            group.lastMessage = lastMessage
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -74,7 +97,6 @@ class ChatFeedTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         if (groupChats.count == 0) {
             let noChatsLabel:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
