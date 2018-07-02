d3f6b1980004121b778989f229ac3dc829ecfde3
Kevin Avila <kevin.avila@utexas.edu>
2018-07-01 23:04:09 -0500

    Added section headers to group settings screen

--- Ellomix/Controllers/GroupSettingsTableViewController.swift
+++ Ellomix/Controllers/GroupSettingsTableViewController.swift
@@ -11,10 +11,47 @@ import UIKit
 class GroupSettingsTableViewController: UITableViewController {
     
     var group: Group!
+    var groupChat: Bool = false
+    let sections = ["Name", "Notifications", "Members", "Leave"]
     
     override func viewDidLoad() {
-        
+        if let count = group.users?.count {
+            if (count > 2) {
+               groupChat = true
+            }
+        }
     }
     
     //Mark: Table View functions
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if (groupChat) {
+            return sections.count
+        }
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if (groupChat) {
+            if (sections[section] == "Leave") {
+                return nil
+            }
+            let  headerCell = tableView.dequeueReusableCell(withIdentifier: "groupSettingsHeaderCell") as! GroupSettingsHeaderCell
+            headerCell.sectionTitleLabel.text = sections[section]
+            
+            return headerCell
+        }
+        let  headerCell = tableView.dequeueReusableCell(withIdentifier: "groupSettingsHeaderCell") as! GroupSettingsHeaderCell
+        headerCell.sectionTitleLabel.text = sections[1]
+        
+        return headerCell
+    }
 }
