dc1f219a3ade1ea66020454718671e6a66fdbbfc
Kevin Avila <kevin.avila@utexas.edu>
2018-07-02 23:52:07 -0500

    Added leave group button

--- Ellomix/Controllers/GroupSettingsTableViewController.swift
+++ Ellomix/Controllers/GroupSettingsTableViewController.swift
@@ -20,15 +20,37 @@ class GroupSettingsTableViewController: UITableViewController {
                groupChat = true
             }
         }
+        
+        let nib = UINib(nibName: "SimpleTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "simpleCell")
     }
     
     //Mark: Table View functions
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (groupChat && sections[section] == "Members") {
+            return (group.users?.count)!
+        }
+        
         return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        if (!groupChat) {
+            return UITableViewCell()
+        }
+        
+        if (sections[indexPath.section] == "Name") {
+            return UITableViewCell()
+        } else if (sections[indexPath.section] == "Notifications") {
+            return UITableViewCell()
+        } else if (sections[indexPath.section] == "Members") {
+            return UITableViewCell()
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "simpleCell", for: indexPath) as! SimpleTableViewCell
+            cell.label.text = "Leave Group"
+
+            return cell
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -41,9 +63,6 @@ class GroupSettingsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if (groupChat) {
-            if (sections[section] == "Leave") {
-                return nil
-            }
             let  headerCell = tableView.dequeueReusableCell(withIdentifier: "groupSettingsHeaderCell") as! GroupSettingsHeaderCell
             headerCell.sectionTitleLabel.text = sections[section]
             
@@ -53,5 +72,13 @@ class GroupSettingsTableViewController: UITableViewController {
         headerCell.sectionTitleLabel.text = sections[1]
         
         return headerCell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if (sections[section] == "Leave") {
+            return 0
+        }
+        
+        return 18
     }
 }
