fb8d255203eac62db2ac6d8cedbece31c804d217
Kevin Avila <kevin.avila@utexas.edu>
2018-08-14 20:39:00 -0500

    Fixed bug where no results view for people was showing when no text was entered. Added ability to dismiss popup player by swiping.

--- Ellomix/Controllers/PopUpPlayerController.swift
+++ Ellomix/Controllers/PopUpPlayerController.swift
@@ -16,6 +16,7 @@ class PopUpPlayerController: UIViewController {
     @IBOutlet weak var artworkImage: UIImageView!
     var currentTrack: Any?
     var playbar: PlayBarController?
+    var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
     
     override func viewDidLoad() {
 
@@ -68,8 +69,27 @@ class PopUpPlayerController: UIViewController {
             print("Unable to play or pause current track.")
         }
     }
-
-
+    
+    @IBAction func panGestureHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        
+        if (sender.state == UIGestureRecognizerState.began) {
+            initialTouchPoint = touchPoint
+        } else if (sender.state == UIGestureRecognizerState.changed) {
+            if (touchPoint.y - initialTouchPoint.y > 0) {
+                self.view.frame = CGRect(x: 0, y: touchPoint.y - initialTouchPoint.y, width: self.view.frame.size.width, height: self.view.frame.size.height)
+            }
+        } else if (sender.state == UIGestureRecognizerState.ended || sender.state == UIGestureRecognizerState.cancelled) {
+            if (touchPoint.y - initialTouchPoint.y > 100) {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+                })
+            }
+        }
+    }
+    
     @IBAction func dismissPlayer(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
