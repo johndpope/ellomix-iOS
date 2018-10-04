feb0dfecd6172f0e69d2c98435264b2ad75afce0
Kevin Avila <kevin.avila@utexas.edu>
2018-10-03 23:36:30 -0500

    Created group table view cell xib

---
+++ Ellomix/Views/Cells/GroupTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GroupTableViewCell.swift
+//  Ellomix
+//
+//  Created by Kevin Avila on 10/3/18.
+//  Copyright © 2018 Akshay Vyas. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var topRightImageView: UIImageView!
+    @IBOutlet weak var bottomLeftImageView: UIImageView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var groupNameLabel: UILabel!
+    
+}
