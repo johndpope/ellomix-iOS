2cbc55f256d4d97aaef9526c0afbdb12ce00f0db
Kevin Avila <kevin.avila@utexas.edu>
2019-02-15 00:10:47 -0600

    Added track title and artist name to SharePost screen. Implemented caption textview and animation. Added autolayout.

--- Ellomix/Controllers/SharePostController.swift
+++ Ellomix/Controllers/SharePostController.swift
@@ -12,6 +12,8 @@ import Firebase
 class SharePostController: UIViewController {
     
     @IBOutlet weak var artworkImage: UIImageView!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var captionTextView: UITextView!
     
     private var FirebaseAPI: FirebaseApi!
@@ -20,8 +22,44 @@ class SharePostController: UIViewController {
     override func viewDidLoad() {
         FirebaseAPI = FirebaseApi()
         
+        captionTextView.textColor = UIColor.lightGray
+        captionTextView.text = "Write a caption..."
+        
+        trackTitleLabel.text = track.title
+        artistLabel.text = track.artist
         if let thumbnailUrl = track.thumbnailURL {
             artworkImage.downloadedFrom(link: thumbnailUrl)
+        }
+        
+        hideKeyboardWhenTappedAround()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    //MARK: Keyboard handling
+    func handleKeyboardNotification(notification: Notification) {
+        let userInfo = notification.userInfo
+        let keyboardFrame = (userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        if (notification.name == Notification.Name.UIKeyboardWillShow) {
+            view.frame.origin.y = -keyboardFrame.height
+            if (captionTextView.textColor == UIColor.lightGray) {
+                captionTextView.text = nil
+                captionTextView.textColor = UIColor.black
+            }
+        } else {
+            view.frame.origin.y = 0
+            if (captionTextView.text.isEmpty) {
+                captionTextView.text = "Write a caption..."
+                captionTextView.textColor = UIColor.lightGray
+            }
         }
     }
 }
